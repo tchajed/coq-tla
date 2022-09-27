28650be594187937c47353f1061df49c39ea7663
Tej Chajed <tchajed@vmware.com>
2022-09-27 11:54:27 -0700

    Start writing more detailed documentation

--- src/defs.v
+++ src/defs.v
@@ -4,8 +4,20 @@ From Coq.Logic Require Import
 From Coq.ssr Require Export ssreflect.
 From stdpp Require Export base.
 
+(*|
+================================
+Embedding the TLA logic in Coq.
+================================
+
+A TLA formula is defined using `predicate`, which is defined in Coq as a predicate over executions (also known as "behaviors" in TLA). Executions are in turn defined to be an infinite sequence of states, which come from some arbitrary type Σ. Note that throughout the entire theory Σ does not change and remains abstract - TLA never talks about how to relate predicates defined for different states.
+|*)
+
 Definition exec (Σ: Type) := nat → Σ.
 Definition predicate (Σ: Type) := exec Σ → Prop.
+
+(*|
+There are a few primitive ways to construct TLA predicates, by lifting state predicates or actions.
+|*)
 Definition state_pred {Σ: Type} (f: Σ → Prop) : predicate Σ :=
     λ ex, f (ex 0).
 
@@ -15,52 +27,71 @@ Definition action_pred {Σ: Type} (a: action Σ) : predicate Σ :=
 
 Notation "⟨ a ⟩" := (action_pred a).
 
+Set Default Proof Using "Type".
+Set Default Goal Selector "!".
+
 Section TLA.
 
 Context {Σ: Type}.
 
-(* this trick avoids needing to repeat the parameters to exec and predicate
-throughout *)
+(* This trick avoids needing to repeat the parameters to [exec] and [predicate] throughout. *)
 Local Notation exec := (exec Σ).
 Local Notation predicate := (predicate Σ).
 
+(* This directive tells Coq that if a variable uses this name (or the name
+followed by numbers or prime symbols), it should automatically get the
+appropriate type. This follows common mathematical presentations, and can make
+things easier to read since types reliably follow naming conventions. *)
 Implicit Types (e: exec) (p q: predicate).
 Implicit Types (n m k: nat).
 
-
-Lemma predicate_ext p1 p2 : (∀ e, p1 e ↔ p2 e) → p1 = p2.
-Proof.
-  intros H.  extensionality e.  apply propositional_extensionality.
-  auto.
-Qed.
-
-Definition valid p := ∀ e, p e.
-
-Definition pred_impl (p q: predicate) :=
-  ∀ e, p e → q e.
+(*|
+TLA lifts the basic propositional logic connectives to predicates over executions, in a straightforward way.
+|*)
 
 Definition tla_not p : predicate := λ e, ¬ p e.
-
 Definition tla_or p1 p2 : predicate := λ e, p1 e ∨ p2 e.
-
 Definition tla_and p1 p2 : predicate := λ e, p1 e ∧ p2 e.
-
 Definition tla_implies p1 p2 : predicate := λ e, p1 e → p2 e.
 
-Lemma pred_impl_as_valid p q :
-  (pred_impl p q) ↔ (valid (tla_implies p q)).
-Proof. reflexivity. Qed.
+(*|
+The heart of TLA's assertions are the `always` and `eventually` modalities. A modality is in general a transformation on formulas. `always p` (which will be written □ p) says that `p` holds "from now on". This is expressed using the `drop` function, which shifts a behavior by k steps.
+
+The other important modality is `eventually p` (which will be written ◇ p).
+|*)
 
 Definition drop k e : exec := λ n, e (n + k).
-
 Definition always p : predicate := λ e, ∀ k, p (drop k e).
-
 Definition eventually p : predicate := λ e, ∃ k, p (drop k e).
 
 (* This serves the rule of the "prime" in TLA, but with a more general and
 formal definition than TLA, which seems to only use them in actions and does not
 treat it as a full-fledged modality. *)
 Definition next p : predicate := λ e, p (drop 1 e).
+
+(*|
+`valid` asserts that a TLA formula "holds" or "is true", which is defined as holding for all executions. This is sometimes phrased as saying that `p` is a tautology, but that can be confusing if you're not used to it. We'll use the standard logic notation `⊢ p` to state that `p` is valid. Note that validity is a "meta language assertion" (Prop, since Coq is our meta language), not a TLA formula.
+
+`pred_impl` is equivalent to `valid (tla_implies p q)`, but it's convenient for Coq to have a definition for it (maybe, I haven't actually tried getting rid of it).
+|*)
+
+Definition valid p := ∀ e, p e.
+
+Definition pred_impl (p q: predicate) :=
+  ∀ e, p e → q e.
+
+(*|
+We assume some axioms so that predicates can be proven equal in the `=` sense if they are logically equivalent, which simplifies working with equivalent predicates a bit.
+|*)
+Lemma predicate_ext p1 p2 : (∀ e, p1 e ↔ p2 e) → p1 = p2.
+Proof.
+  intros H.  extensionality e.  apply propositional_extensionality.
+  auto.
+Qed.
+
+Lemma pred_impl_as_valid p q :
+  (pred_impl p q) ↔ (valid (tla_implies p q)).
+Proof. reflexivity. Qed.
 
 Lemma equiv_to_impl p q :
   (pred_impl p q) → (pred_impl q p) → (p = q).
@@ -95,6 +126,10 @@ Notation "p  →  q" := (tla_implies p%L q%L) : tla.
 Notation "□  p" := (always p%L) (at level 51, right associativity) : tla.
 Notation "◇  p" := (eventually p%L) (at level 51, right associativity) : tla.
 
+(*|
+`weak_fairness` is a useful notion for liveness. When `weak_fairness a` holds of a behavior, if at any point the action is always enabled, then it must eventually run.
+|*)
+
 (* these two definitions are here so they can use the notation *)
 
 Definition enabled {Σ} (a: action Σ) : predicate Σ :=
@@ -112,9 +147,11 @@ Ltac deex :=
   | _ => subst
   end.
 
-(* These instances enable rewriting by predicate implications, even inside
+(*|
+The instances blow enable rewriting by predicate implications, even inside
 formulae. For example we can use H: p ⊢ q (pred_impl p q) as [rewrite H] to
-change [p ∧ r ⊢ q] to [q ∧ r ⊢ q]. *)
+change [p ∧ r ⊢ q] to [q ∧ r ⊢ q].
+|*)
 Section rewriting.
 
 Context [Σ: Type].
