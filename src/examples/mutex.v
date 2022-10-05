1ef1900879731459a9c586e3aa6b2d6acacd354c
Tej Chajed <tchajed@vmware.com>
2022-10-05 11:43:28 -0700

    Add some documentation to mutex

--- src/examples/mutex.v
+++ src/examples/mutex.v
@@ -131,7 +131,6 @@ Ltac stm_simp :=
   intros; (intuition (repeat deex; subst; trivial));
   rewrite ?enabled_eq ?enabled_thread;
   repeat deex;
-  (* TODO: why does this infinite loop? *)
   repeat (match goal with
         | s: state |- _ => (destruct s; cbn in * )
         | H: ?x = ?x |- _ => clear H
@@ -188,7 +187,80 @@ Proof.
     execution which is just the initial state and then infinite stuttering *)
 Abort.
 
-(* TODO: document these two proofs *)
+(** wrapper for [exclusion_inv_ok] to "upgrade" from a semantics with [next] to
+one that incorporates the invariant *)
+Lemma add_safety :
+  ⌜init⌝ ∧ □⟨next⟩ ⊢
+  ⌜init⌝ ∧ □⟨λ s s', next s s' ∧ exclusion_inv s ∧ exclusion_inv s'⟩.
+Proof.
+  tla_pose (exclusion_inv_ok).
+  rewrite combine_preds //.
+Qed.
+
+(*|
+
+---------------------
+Proving termination
+---------------------
+
+Why does this program terminate? The basic reasoning in this proof is fairly brute force. However, we must use the assumption of weak fairness in order to guarantee that each thread makes progress (as opposed to stuttering or starving). The main rule is wf1, which gives a `~~>` from weak fairness of an action.
+
+The tricky part about the mutex example is getting started, because we don't know whether thread A or thread B will acquire the lock first, and thus we're not sure whether the action `step tidA` will happen or `step tidB`. The way we resolve this in the proof is to show that `step tidA` eventually runs and results in one of two possibilities: either tidA acquires the lock (and moves on to `pc1`), or it fails to acquire the lock. Now the crucial reasoning step is that if tidA doesn't acquire the lock, then tidB must hold it. In general we only know that tidB is in its critical section, but in fact because the state machine is so trivial that the entire critical section is just `pc1`.
+
+Now we have a "diverging" leads to, which leads to one of two possibilities. TLA gives a way to construct a single leads-to from a "lattice" of leads-tos, as long as the eventually converge to some goal state. In this example, once either tidA or tidB acquire the lock, the remaining execution is sequential, so the divergence is very simple. As a result we use `leads_to_fork2`, a very special case of the lattice rule that just gives a way to diverge to `q ∨ r` and then reconverge to some goal.
+
+|*)
+
+Lemma init_to_terminate :
+  ⌜init⌝ ∧ □⟨next⟩ ∧ fair ⊢ ⌜init⌝ ~~> ⌜terminated⌝.
+Proof.
+  unfold fair.
+(*|
+This manipulation (which unfortunately is messy because things are so syntactic) brings the `exclusion_inv` invariant into scope. We need this because the intermediate assertions in this proof don't all mention the lock state, and the invariant helps us infer it from the program counters of the two threads. (A more precise invariant could avoid all mention of locks.)
+|*)
+  rewrite <- tla_and_assoc. rewrite -> add_safety. tla_simp.
+
+(*|
+The first creative step is to lay out what two possibilities are possible from `init`. As described above, either thread A acquires the lock, or B is in its critical section and A is waiting.
+|*)
+  apply (leads_to_fork2
+           ⌜λ s, s.(pcs) tidA = pc1 ∧ s.(pcs) tidB = pc0⌝
+           ⌜λ s, s.(pcs) tidA = pc0 ∧ s.(pcs) tidB = pc1⌝).
+(*|
+This is actually the non-obvious initial divergence where tidA runs and either succeeds or fails. Automation makes short work of it. Note that we could just as well get this result from thread B.
+|*)
+  - rewrite combine_or_preds.
+    tla_apply (wf1 (step tidA)); stm.
+
+  -
+(*|
+Next we reason through a sequence of states, connected by wf1. First A steps and releases the lock, then B steps twice to acquire and release.
+|*)
+
+    leads_to_trans ⌜λ s, s.(pcs) tidA = pc2 ∧
+                         s.(pcs) tidB = pc0 ∧
+                         s.(lock) = false⌝.
+    { tla_apply (wf1 (step tidA)); stm. }
+    leads_to_trans ⌜ λ s, s.(pcs) tidA = pc2 ∧ s.(pcs) tidB = pc1⌝.
+    { tla_apply (wf1 (step tidB)); stm. }
+    tla_apply (wf1 (step tidB)); stm.
+  -
+(*|
+The other branch is symmetric, with B releasing the lock and then A acquiring and releasing.
+|*)
+    leads_to_trans ⌜λ s, s.(pcs) tidB = pc2 ∧
+                         s.(pcs) tidA = pc0 ∧
+                         s.(lock) = false⌝.
+    { tla_apply (wf1 (step tidB)); stm. }
+    leads_to_trans ⌜ λ s, s.(pcs) tidB = pc2 ∧ s.(pcs) tidA = pc1⌝.
+    { tla_apply (wf1 (step tidA)); stm. }
+    tla_apply (wf1 (step tidA)); stm.
+Qed.
+
+
+(*|
+Below is an alternate proof of the same termination theorem. Instead of using the special case `leads_to_fork2`, we use the lattice rule with a lattice that fairly explicitly gives the states. Some of the paths in this lattice are actually linear chains, which can be optimized away, but maybe in some cases the lattice is easier to think about.
+|*)
 
 Inductive L := start | A1 | Afin | AfinB1 | B1 | Bfin | BfinA1 | goal.
 
@@ -232,15 +304,7 @@ Qed.
 
 Hint Extern 1 (Llt _ _) => exact I : core.
 
-Lemma add_safety :
-  ⌜init⌝ ∧ □⟨next⟩ ⊢
-  ⌜init⌝ ∧ □⟨λ s s', next s s' ∧ exclusion_inv s ∧ exclusion_inv s'⟩.
-Proof.
-  tla_pose (exclusion_inv_ok).
-  rewrite combine_preds //.
-Qed.
-
-Lemma init_to_terminate :
+Lemma init_to_terminate_lattice_proof :
   ⌜init⌝ ∧ □⟨next⟩ ∧ fair ⊢ ⌜init⌝ ~~> ⌜terminated⌝.
 Proof.
   unfold fair.
@@ -285,32 +349,6 @@ Proof.
   - (* BfinA1 *)
     leads_to_trans ⌜terminated⌝; swap 1 2.
     { apply leads_to_helper => s. exists goal; eauto. }
-    tla_apply (wf1 (step tidA)); stm.
-Qed.
-
-Lemma init_to_terminate' :
-  ⌜init⌝ ∧ □⟨next⟩ ∧ fair ⊢ ⌜init⌝ ~~> ⌜terminated⌝.
-Proof.
-  unfold fair.
-  rewrite <- tla_and_assoc. rewrite -> add_safety. tla_simp.
-  apply (leads_to_fork2
-           ⌜λ s, s.(pcs) tidA = pc1 ∧ s.(pcs) tidB = pc0⌝
-           ⌜λ s, s.(pcs) tidA = pc0 ∧ s.(pcs) tidB = pc1⌝).
-  - rewrite combine_or_preds.
-    tla_apply (wf1 (step tidA)); stm.
-  - leads_to_trans ⌜λ s, s.(pcs) tidA = pc2 ∧
-                         s.(pcs) tidB = pc0 ∧
-                         s.(lock) = false⌝.
-    { tla_apply (wf1 (step tidA)); stm. }
-    leads_to_trans ⌜ λ s, s.(pcs) tidA = pc2 ∧ s.(pcs) tidB = pc1⌝.
-    { tla_apply (wf1 (step tidB)); stm. }
-    tla_apply (wf1 (step tidB)); stm.
-  - leads_to_trans ⌜λ s, s.(pcs) tidB = pc2 ∧
-                         s.(pcs) tidA = pc0 ∧
-                         s.(lock) = false⌝.
-    { tla_apply (wf1 (step tidB)); stm. }
-    leads_to_trans ⌜ λ s, s.(pcs) tidB = pc2 ∧ s.(pcs) tidA = pc1⌝.
-    { tla_apply (wf1 (step tidA)); stm. }
     tla_apply (wf1 (step tidA)); stm.
 Qed.
 
