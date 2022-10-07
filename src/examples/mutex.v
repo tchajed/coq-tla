5327b08f65c2f8d366214fd9f8efe63a43dc9c65
Tej Chajed <tchajed@vmware.com>
2022-10-07 15:32:04 -0700

    Prove safety for mutex

--- src/examples/mutex.v
+++ src/examples/mutex.v
@@ -44,6 +44,11 @@ Module spec.
     Inductive t :=
       lock_cas | futex_wait | kernel_wait |
       unlock_store | unlock_wake | finished.
+
+    Notation start := lock_cas (only parsing).
+
+    Notation critical_sec := unlock_store (only parsing).
+
   End pc.
 
   Definition Tid := nat.
@@ -65,8 +70,7 @@ Module spec.
     | _::xs => xs
     end.
 
-  Definition thread_step (t: Tid) : action (State * pc.t) :=
-    λ '(σ, pc) '(σ', pc'),
+  Definition thread_step_def (t: Tid) σ pc σ' pc' :=
     match pc with
     | pc.lock_cas =>
         (* cas success *)
@@ -91,7 +95,7 @@ Module spec.
         in the kernel *)
         t ∉ σ.(queue) ∧
         σ' = σ ∧
-        pc' = pc.unlock_store
+        pc' = pc.lock_cas
     | pc.unlock_store =>
         σ' = σ<|lock := false|> ∧
         pc' = pc.unlock_wake
@@ -101,23 +105,26 @@ Module spec.
     | pc.finished => False
     end.
 
+  Definition thread_step (t: Tid) : action (State * pc.t) :=
+    λ '(σ, pc) '(σ', pc'), thread_step_def t σ pc σ' pc'.
+
   Record Config :=
     mkConfig { state: State;
                 (* program counter for each thread (or None if thread never
                 existed) *)
-                pcs: gmap Tid pc.t; }.
+                tp: gmap Tid pc.t; }.
 
   #[global]
-  Instance _eta_config : Settable _ := settable! mkConfig <state; pcs>.
+  Instance _eta_config : Settable _ := settable! mkConfig <state; tp>.
 
   Definition step (t: Tid) : action Config :=
-    λ s s', ∃ pc, s.(pcs) !! t = Some pc ∧
+    λ s s', ∃ pc, s.(tp) !! t = Some pc ∧
             ∃ ρ', thread_step t (s.(state), pc) ρ' ∧
-                      s' = mkConfig ρ'.1 (<[t := ρ'.2]> s.(pcs)).
+                      s' = mkConfig ρ'.1 (<[t := ρ'.2]> s.(tp)).
 
   Definition init : Config → Prop :=
     λ s, s.(state) = {| lock := false; queue := [] |} ∧
-         ∀ tid pc, s.(pcs) !! tid = Some pc → pc = pc.lock_cas.
+         ∀ tid pc, s.(tp) !! tid = Some pc → pc = pc.start.
 
   Definition next : action Config :=
     λ s s', (∃ tid, step tid s s') ∨ s' = s.
@@ -128,9 +135,30 @@ Module spec.
   Definition spec : predicate Config :=
     ⌜init⌝ ∧ □⟨next⟩ ∧ fair.
 
+(*|
+**Safety** means mutual exclusion in the critical section, which is a single program counter (the beginning of the unlock function.)
+|*)
+Definition safe: Config → Prop :=
+    λ s, ∀ tid tid',
+    s.(tp) !! tid = Some pc.critical_sec →
+    s.(tp) !! tid' = Some pc.critical_sec →
+    tid = tid'.
+
+(*|
+**Liveness** means all threads have terminated.
+|*)
+  Definition terminated: Config → Prop :=
+    λ s, ∀ tid pc, s.(tp) !! tid = Some pc → pc = pc.finished.
+
 End spec.
 
 Import spec.
+
+Section example.
+
+Implicit Types (σ: State) (s: Config) (t: Tid).
+Implicit Types (ρ: State * pc.t).
+Implicit Types (l: bool) (q: list Tid).
 
 Lemma state_inv l1 q1 l2 q2 :
   mkState l1 q1 = mkState l2 q2 ↔ l1 = l2 ∧ q1 = q2.
@@ -140,6 +168,11 @@ Proof.
   - intuition congruence.
 Qed.
 
+Lemma thread_step_eq t σ pc σ' pc' :
+  thread_step t (σ, pc) (σ', pc') ↔ thread_step_def t σ pc σ' pc'.
+Proof. reflexivity. Qed.
+Opaque thread_step.
+
 (* sanity check on semantics *)
 Lemma thread_step_deterministic t ρ :
   ∀ ρ' ρ'' ,
@@ -148,7 +181,9 @@ Lemma thread_step_deterministic t ρ :
   ρ' = ρ''.
 Proof.
   destruct ρ as [[l q] pc].
-  destruct pc; intros [[l' q'] pc] [[l'' q''] pc'']; simpl;
+  intros [[l' q'] pc'] [[l'' q''] pc''].
+  rewrite ?thread_step_eq.
+  destruct pc; simpl;
     rewrite ?state_inv;
     try intuition congruence.
 Qed.
@@ -160,50 +195,193 @@ Proof. naive_solver. Qed.
 
 Lemma thread_step_enabled t :
   enabled (thread_step t) =
-    (λ σ, (σ.2 = pc.kernel_wait → t ∉ σ.1.(queue)) ∧
-          (σ.2 = pc.finished → False)).
+    (λ ρ, (ρ.2 = pc.kernel_wait → t ∉ ρ.1.(queue)) ∧
+          (ρ.2 = pc.finished → False)).
 Proof.
   apply pred_ext; intros [σ pc]; simpl.
   unfold enabled.
   split.
   - intros [[σ' pc'] Hstep].
+    rewrite thread_step_eq in Hstep.
     destruct pc; simpl in *; try (intuition congruence).
   - intros [? ?].
     apply exist_prod.
+    setoid_rewrite thread_step_eq.
     destruct pc; simpl; intuition eauto.
     * destruct (lock σ); eauto 8.
     * destruct (lock σ); eauto 8.
 Qed.
 
-Lemma thread_step_enabled_s t σ :
-  enabled (thread_step t) σ ↔
-    (σ.2 = pc.kernel_wait → t ∉ σ.1.(queue)) ∧
-    (σ.2 = pc.finished → False).
+Lemma thread_step_enabled_s t ρ :
+  enabled (thread_step t) ρ ↔
+    (ρ.2 = pc.kernel_wait → t ∉ ρ.1.(queue)) ∧
+    (ρ.2 = pc.finished → False).
 Proof.
   rewrite thread_step_enabled //.
 Qed.
 
 Lemma step_enabled t :
   enabled (step t) =
-    (λ s, ∃ pc, s.(pcs) !! t = Some pc ∧
+    (λ s, ∃ pc, s.(tp) !! t = Some pc ∧
                 (pc = pc.kernel_wait → t ∉ s.(state).(queue)) ∧
                 (pc ≠ pc.finished)).
 Proof.
-  apply pred_ext; intros [σ pcs]; simpl.
-  unfold enabled, step; cbn [spec.pcs set state].
+  apply pred_ext; intros [σ tp]; simpl.
+  unfold enabled, step; simpl.
   split.
   - intros; repeat deex.
     rewrite H. eexists; split; [ by auto | ].
     destruct ρ' as [σ' pc'].
     pose proof (thread_step_enabled_s t (σ, pc)) as [Htenable _];
-      cbn [fst snd] in *.
+      simpl in *.
     apply Htenable.
     eexists; eauto.
   - intros; repeat deex.
     rewrite H.
     pose proof (thread_step_enabled_s t (σ, pc)) as [_ Htenable];
-      cbn [fst snd] in *.
+      simpl in *.
     intuition auto.
     destruct H2 as [ρ' Hstep].
     eauto 8.
 Qed.
+
+Definition exclusion_inv: Config → Prop :=
+  λ s, (∀ tid, s.(tp) !! tid = Some pc.critical_sec →
+               s.(state).(lock) = true) ∧
+       safe s.
+
+#[local]
+Hint Unfold init next step safe fair terminated : stm.
+
+Ltac lookup_simp :=
+  subst;
+  repeat
+    match goal with
+    | H: _ |- _ => rewrite lookup_insert in H
+    | H: _ |- _ => rewrite -> lookup_insert_ne in H by congruence
+    | _ => rewrite lookup_insert
+    | _ => rewrite -> lookup_insert_ne by congruence
+    end;
+  try congruence.
+
+Ltac invc H := inversion H; subst; clear H.
+
+Ltac stm_simp :=
+  autounfold with stm in *;
+  intros; destruct_and?;
+  repeat (match goal with
+        | _ => progress deex
+        | _ => progress subst
+        | _ => destruct_and!
+        | _ => destruct_or!
+        | s: Config |- _ =>
+            let σ := fresh "σ" in
+            let tp := fresh "tp" in
+            destruct s as [σ tp]; cbn in *
+        | σ: State |- _ =>
+            let l := fresh "l" in
+            let q := fresh "q" in
+            destruct σ as [l q]; cbn in *
+        | ρ: State * pc.t |- _ =>
+            let σ := fresh "σ" in
+            let pc := fresh "pc" in
+            destruct ρ as [σ pc]; cbn in *
+        | H: ?x = ?x |- _ => clear H
+        | H: @eq pc.t _ _ |- _ => solve [ inversion H ]
+        | H: @eq _ (mkState _ _) (mkState _ _) |- _ =>
+            invc H; cbn in *
+        | H: @eq _ (mkConfig _ _) (mkConfig _ _) |- _ =>
+            invc H; cbn in *
+        | H: Some _ = Some _ |- _ => invc H
+        | H: context[@set state _ _ _ _ _] |- _ =>
+            progress (unfold set in H; simpl in H)
+        | H: @eq bool _ _ |- _ => solve [ inversion H ]
+        | _ => progress (unfold set; simpl)
+        | _ => progress lookup_simp
+        | _ => progress cbn in *
+        end).
+
+Ltac stm :=
+  stm_simp;
+  try solve [ intuition (repeat deex; eauto) ];
+  try congruence;
+  repeat first [
+      (split; [ solve [ intuition eauto ] | ])
+    | (split; [ | solve [ intuition eauto ] ])
+    ].
+
+Hint Unfold exclusion_inv : stm.
+
+Lemma insert_lookup_dec {tp: gmap Tid pc.t} :
+  ∀ t t' pc',
+  <[t' := pc']> tp !! t = if decide (t = t') then Some pc' else tp !! t.
+Proof.
+  intros.
+  destruct (decide _); lookup_simp.
+Qed.
+
+Theorem exclusion_inv' s :
+  exclusion_inv s ↔
+    (∀ t t', s.(tp) !! t = Some pc.critical_sec →
+             s.(tp) !! t' = Some pc.critical_sec →
+             t = t' ∧ s.(state).(lock) = true).
+Proof.
+  unfold exclusion_inv, safe.
+  split; intros; destruct_and?; eauto.
+  split; intros.
+  - pose proof (H tid tid); intuition eauto.
+  - pose proof (H tid tid'); intuition eauto.
+Qed.
+
+Opaque thread_step.
+
+Lemma exclusion_inv_ok :
+  spec ⊢ □⌜exclusion_inv⌝.
+Proof.
+  unfold spec.
+  tla_clear fair.
+  apply init_invariant.
+  - stm; intuition auto.
+    { pose proof (H1 _ _ H); congruence. }
+    { pose proof (H1 _ _ H); congruence. }
+  - intros [σ tp] [σ' tp']; simpl.
+    intros Hinv Hnext.
+    destruct Hnext as [ [t Hstep] | Heq]; [ | stm_simp; by eauto ].
+    destruct Hstep as [pc'' [Hlookup [ρ' [Hstep Heq]]]].
+    simpl in *.
+    invc Heq.
+    destruct ρ' as [σ' pc']; simpl.
+
+    apply exclusion_inv' => t' t'' /= Ht' Ht''.
+    destruct Hinv as [Hlocked Hsafe]; unfold safe in *; simpl in *.
+    rewrite thread_step_eq /thread_step_def in Hstep.
+    destruct (decide (t' = t'')); subst.
+    { split; first done.
+      destruct (decide (t = t'')); lookup_simp.
+      - invc Ht'.
+        destruct pc''; intuition (subst; eauto; try congruence).
+      - invc Ht'.
+        destruct pc''; intuition (subst; eauto; try congruence). }
+
+    destruct (decide (t = t''));
+      destruct (decide (t = t'));
+      subst; lookup_simp; eauto;
+      assert (σ.(lock) = true) by eauto.
+    { invc Ht''.
+      destruct pc''; intuition (subst; eauto);
+        try congruence. }
+    { invc Ht''.
+      destruct pc''; intuition (subst; eauto);
+        try congruence. }
+    split; eauto.
+Qed.
+
+Theorem safety :
+  spec ⊢ □ ⌜safe⌝.
+Proof.
+  rewrite -> exclusion_inv_ok.
+  apply always_impl_proper.
+  unseal; stm.
+Qed.
+
+End example.
