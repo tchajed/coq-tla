b60ca5a60933b6740110235633cde62c6b454de2
Tej Chajed <tchajed@vmware.com>
2022-10-10 16:44:36 -0700

    Print projections in intermediate goals

--- src/prelude.v
+++ src/prelude.v
@@ -3,6 +3,7 @@ From stdpp Require Export base.
 
 #[global] Set Default Proof Using "Type".
 #[global] Set Default Goal Selector "!".
+#[global] Set Printing Projections.
 (* re-enable [if] on non-booleans *)
 #[global] Open Scope general_if_scope.
 
